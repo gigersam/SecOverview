852dc4d03760e66c5b2feb726b025728aaa9c763
Samuel Giger <gigersam@gmail.com>
2025-02-16 03:40:12 +0100

    changed default key value for replacment

--- secoverview/secoverview/settings.py
+++ secoverview/secoverview/settings.py
@@ -24,7 +24,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'KEY_VALUE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
