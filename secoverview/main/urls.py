49e808c8b1f15ddfbd5a78485d46a0c8a3a4ffe6
Samuel Giger <gigersam@gmail.com>
2025-02-17 21:55:12 +0100

    Updated Documentation and changed default url / to dashboard view

--- secoverview/main/urls.py
+++ secoverview/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from dashboard.views import dashboard
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', dashboard, name='index'),
     path('about/', views.about, name='about'),
 ]
